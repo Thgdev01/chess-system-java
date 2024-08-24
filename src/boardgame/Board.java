7eee06ff1b94da0ceeef62598e09bd221951e0e6
Thiag0 <thiagodealmeidamelo@gmail.com>
2024-08-23 21:37:21 -0300

    BoardException e programação defensiva

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -1,48 +1,68 @@
 package boardgame;
 
 public class Board {
-	 
+
 	private int rows;
 	private int columns;
 	private Piece[][] pieces;
-	
-	
-	public Board(int rows, int columns) {
-		this.rows = rows;
-		this.columns = columns;
-		pieces = new Piece[rows][columns];
-	}
 
+	public Board(int rows, int columns) {
+		if (rows < 1 || columns < 1) {
+			throw new BoardException("Erro ao criar tabuleiro: ele deve ter pelo menos uma linha e uma coluna!");
+		} else {
+			this.rows = rows;
+			this.columns = columns;
+			pieces = new Piece[rows][columns];
+		}
+	}
 
 	public int getRows() {
 		return rows;
 	}
 
-
-	public void setRows(int rows) {
-		this.rows = rows;
-	}
-
-
 	public int getColumns() {
 		return columns;
 	}
 
-
-	public void setColumns(int columns) {
-		this.columns = columns;
-	}
-
 	public Piece piece(int row, int column) {
-		return pieces[row][column];
+		if (!positionExists(row, column)) {
+			throw new BoardException("Posição não está correta em relação ao tabuleiro");
+		} else {
+			return pieces[row][column];
+		}
 	}
-	
+
 	public Piece piece(Position position) {
-		return pieces[position.getRow()][position.getColumn()];
+		if (!positionExists(position)) {
+			throw new BoardException("Posição não está correta em relação ao tabuleiro");
+		} else {
+
+			return pieces[position.getRow()][position.getColumn()];
+		}
 	}
-	
+
 	public void placePiece(Piece piece, Position position) {
-		pieces[position.getRow()][position.getColumn()] = piece;
-		piece.position = position;
+		if (thereIsAPiece(position)) {
+			throw new BoardException("Já contém uma peça na posição " + position);
+		} else {
+			pieces[position.getRow()][position.getColumn()] = piece;
+			piece.position = position;
+		}
+	}
+
+	public boolean positionExists(int row, int column) {
+		return row >= 0 && row < rows && column >= 0 && column < columns;
+	}
+
+	public boolean positionExists(Position position) {
+		return positionExists(position.getRow(), position.getColumn());
+	}
+
+	public boolean thereIsAPiece(Position position) {
+		if (!positionExists(position)) {
+			throw new BoardException("Posição não está correta em relação ao tabuleiro");
+		} else {
+			return piece(position) != null;
+		}
 	}
 }
